c7c9bc44e6404c12f63498f352d78042173c5c05
Alex Kreskiyan <a.kreskiyan@gmail.com>
2023-01-31 14:00:01 +0300

    update deps

--- server/src/Server.Db/ServerConnection.cs
+++ server/src/Server.Db/ServerConnection.cs
@@ -2,10 +2,11 @@ using Annium.linq2db.Extensions.Models;
 using Annium.Logging.Abstractions;
 using Core.Domain.Models;
 using LinqToDB;
+using LinqToDB.Data;
 
 namespace Server.Db;
 
-public class ServerConnection : DataConnectionBase, ILogSubject<ServerConnection>
+public class ServerConnection : DataConnection, ILogSubject<ServerConnection>
 {
     public ILogger<ServerConnection> Logger { get; }
     public ITable<User> Users { get; }
